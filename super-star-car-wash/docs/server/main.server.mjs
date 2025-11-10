5c6bf8c5528856ac40c7bca026ebdfb3eeb94167
Shaik Mohammad <shahabaz.bano.shaik@accenture.com>
2025-11-11 01:42:50 +0530

    Fix index.html + assets path; deploy

---
+++ super-star-car-wash/docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-TU2H6P6Q.mjs";import"./chunk-PO2EBPCH.mjs";import"./chunk-JJV5WH3V.mjs";import"./chunk-XTPRZG6S.mjs";import"./chunk-ADXVGYZU.mjs";import"./chunk-RA7DYA2A.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
---
+++ super-star-car-wash/docs/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{c as o,d as r,e as n}from"./chunk-JJV5WH3V.mjs";import{ib as e}from"./chunk-RA7DYA2A.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
